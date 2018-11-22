bbbcb26357b7931e7f71b079699649b078452756
Francisco Ferreira Cardoso <francisco.cardoso@sp01.local>
2018-11-22 10:52:10 -0200

    correcao ultimo commit

--- validator/test/testOpenAPI.js
+++ validator/test/testOpenAPI.js
@@ -26,7 +26,7 @@ describe("Validating OpenAPI files...", function () {
       // console.log('OPENAPI files');
       // console.log(filenames);
       filenames.forEach(function (filename) {
-        if (filename.includes("JobScheduler_v1_001.json") && !filename.includes("package")) {
+        if (filename.includes("*.json") && !filename.includes("package")) {
           let openAPIPath = path.join(dirname, filename);
 
           describe("OpenAPI - " + filename, function () {
