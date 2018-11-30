6810ec6c9cb8007ea36ff6b8a7aef10b45a38579
Francisco Ferreira Cardoso <francisco.cardoso@sp01.local>
2018-11-30 17:10:35 -0200

    Teste nome da branch passado como parametro

--- travis-renameRef.sh
+++ travis-renameRef.sh
@@ -1,4 +1,6 @@
-node validator/master/renameRef.js
-git add -A
-git commit -m "Renaming schema references to branch 'master'"
-git push
+branchname=$1
+echo "branchname: " + branchname
+# node validator/master/renameRef.js
+# git add -A
+# git commit -m "Renaming schema references to branch 'master'"
+# git push origin branchname
