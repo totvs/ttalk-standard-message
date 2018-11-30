88e3eb5275a286562d50f4157c7a8d4360312b42
Francisco Ferreira Cardoso <francisco.cardoso@sp01.local>
2018-11-30 16:35:38 -0200

    renamingontravis

--- validator/master/renameRef.js
+++ validator/master/renameRef.js
@@ -13,7 +13,7 @@ var dirname = "./jsonschema/apis/";
             encoding: 'utf-8'
           });
           parsedOpenAPI = JSON.parse(file);
-          parsedOpenAPI.servers[0].url = "DoingGitCommitsOnShell";
+          parsedOpenAPI.servers[0].url = "RenamingOnTravis";
           stringOpenAPI = JSON.stringify(parsedOpenAPI, null, '\t');
           // for (var pathkey in parsedOpenAPI.paths) {
           // }
