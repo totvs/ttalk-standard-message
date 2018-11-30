e96654a192d2e07b3710029199a8d71ac33130f5
Francisco Ferreira Cardoso <francisco.cardoso@sp01.local>
2018-11-30 17:29:48 -0200

    Credentials setted

--- travis-renameRef.sh
+++ travis-renameRef.sh
@@ -1,9 +1,5 @@
-echo $1
-echo $2
-echo $3
-
-git config --global user.email "$2"
-git config --global user.name "$3"
+git config --global user.email "francardoso@outlook.com"
+git config --global user.name "Francisco Ferreira Cardoso"
 git config --global push.default simple
 
 node validator/master/renameRef.js
