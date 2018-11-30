800751a5a32380812a0363b6b537440d31e7e9ed
Francisco Ferreira Cardoso <francisco.cardoso@sp01.local>
2018-11-30 17:16:12 -0200

    Sera que vai?

--- travis-renameRef.sh
+++ travis-renameRef.sh
@@ -1,5 +1,4 @@
-echo "branchname: $1" 
-# node validator/master/renameRef.js
-# git add -A
-# git commit -m "Renaming schema references to branch 'master'"
-# git push origin branchname
+node validator/master/renameRef.js
+git add -A
+git commit -m "Renaming schema references to branch 'master'"
+git push origin $1
