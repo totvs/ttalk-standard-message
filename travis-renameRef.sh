e68ebe5c2a26efd24dd2bd4a9832ec89a4a27427
Francisco Ferreira Cardoso <francardoso@outlook.com>
2018-11-30 17:36:04 -0200

    Echo

--- travis-renameRef.sh
+++ travis-renameRef.sh
@@ -2,6 +2,8 @@ git config --global user.email "francardoso@outlook.com"
 git config --global user.name "Francisco Ferreira Cardoso"
 git config --global push.default simple
 
+echo "Definidos Git Config"
+
 node validator/master/renameRef.js
 git add -A
 git commit -m "Renaming schema references to branch  'master'"
