054d6fa35e6d1d340af300be9894224ef18536f3
Francisco Ferreira Cardoso <francisco.cardoso@sp01.local>
2018-11-30 17:20:11 -0200

    Verificando se dar pra configurar git credentials com variaveis de ambiente direto no shell

--- travis-renameRef.sh
+++ travis-renameRef.sh
@@ -1,3 +1,11 @@
+echo $TRAVIS_BRANCH
+echo ${GIT_EMAIL}
+echo ${GIT_NAME}
+
+git config --global user.email "${GIT_EMAIL}"
+git config --global user.name "${GIT_NAME}"
+git config --global push.default simple
+
 node validator/master/renameRef.js
 git add -A
 git commit -m "Renaming schema references to branch 'master'"
