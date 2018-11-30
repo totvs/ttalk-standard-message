15de295c0a81eb86f48fb284b08fdcd3370ceab1
Francisco Ferreira Cardoso <francisco.cardoso@sp01.local>
2018-11-30 13:58:30 -0200

    Executando o renameRef em script

---
+++ validator/master/renameRef.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+var path = require('path');
+var dirname = "./jsonschema/apis/";
+    fs.readdir(dirname, function (err, filenames) {
+      if (err) {
+        console.log(err);
+      }
+
+      filenames.forEach(function (filename) {
+        if (filename.includes(".json") && !filename.includes("package")) {
+          let openAPIPath = path.join(dirname, filename);
+          var file = fs.readFileSync(openAPIPath, {
+            encoding: 'utf-8'
+          });
+          parsedOpenAPI = JSON.parse(file);
+          parsedOpenAPI.servers[0].url = "empty";
+          stringOpenAPI = JSON.stringify(parsedOpenAPI, null, '\t');
+          // for (var pathkey in parsedOpenAPI.paths) {
+          // }
+          console.log(dirname + filename);
+          fs.writeFileSync(dirname + filename, stringOpenAPI);
+        }
+    })
+})
