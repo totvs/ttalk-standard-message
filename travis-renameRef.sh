6ac1d26de102ffd9e536850055bf2a795679fa9f
Francisco Ferreira Cardoso <francardoso@outlook.com>
2018-12-03 14:22:41 -0200

    Sleep and git status

--- travis-renameRef.sh
+++ travis-renameRef.sh
@@ -9,7 +9,10 @@ git config --global user.name "francardoso93"
 echo "running travis-renameRef.sh"
 
 node validator/master/renameRef.js
+sleep 30s 
+
 git add -A
+git status
 git commit -m "TRAVISCI - Renaming schema references to branch  'master'"
 git remote add origin-pages https://${GH_TOKEN}@github.com/totvs/ttalk-standard-message.git > /dev/null 2>&1                        
 git push --set-upstream origin-pages ${TRAVIS_BRANCH}
