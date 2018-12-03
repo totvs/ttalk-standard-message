36113d19ea6acdb8f1a0387d1b0daa3ffa3ce40b
Francisco Ferreira Cardoso <francardoso@outlook.com>
2018-12-03 08:38:35 -0200

    Com accesstoken vai?

--- travis-renameRef.sh
+++ travis-renameRef.sh
@@ -1,17 +1,18 @@
 git config --global user.email "francardoso@outlook.com"
 git config --global user.name "Francisco Ferreira Cardoso"
-git config --global push.default simple
 
 echo git config -l
 
 #https://stackoverflow.com/questions/10054318/how-to-provide-username-and-password-when-run-git-clone-gitremote-git
 #https://username:password@github.com/username/repository.git
-git config remote.origin.url francardoso93:25789t@T@github.com/totvs/ttalk-standard-message.git
+#https://gist.github.com/willprice/e07efd73fb7f13f917ea
+#git config remote.origin.url b50c25892bd9411e85c7da30767bfc90581d23bc@github.com/totvs/ttalk-standard-message.git
 
 echo "eee Git Config e origin"
 
 node validator/master/renameRef.js
 git add -A
-git commit -m "Renaming schema references to branch  'master'"
+git commit -m "TRAVISCI - Renaming schema references to branch  'master'"
+git remote add origin-pages b50c25892bd9411e85c7da30767bfc90581d23bc@github.com/totvs/ttalk-standard-message.git
 git push origin $1
 
