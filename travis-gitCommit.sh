9aeb997c2203bd68a80072614eaa1d426a99e304
Francisco Ferreira Cardoso <francardoso@outlook.com>
2018-12-03 15:45:23 -0200

    try again

--- travis-gitCommit.sh
+++ travis-gitCommit.sh
@@ -5,7 +5,6 @@ git checkout ${TRAVIS_BRANCH}
 git pull                   
 
 git add -A
-git status
 git commit -m "TRAVISCI - Renaming schema references to branch  'master' - ${TRAVIS_BUILD_NUMBER}"
 git remote add origin-pages https://${GH_TOKEN}@github.com/totvs/ttalk-standard-message.git > /dev/null 2>&1     
 git push --set-upstream origin-pages 
