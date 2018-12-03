f94336dd3cf90270228084d084639e270270e96f
Francisco Ferreira Cardoso <francardoso@outlook.com>
2018-12-03 09:36:13 -0200

    Testando definicao /dev/null, variavel de ambiente travis_branch direto no comando do push origin e --quiet

--- travis-renameRef.sh
+++ travis-renameRef.sh
@@ -1,15 +1,13 @@
 git config --global user.email "francardoso@outlook.com"
 git config --global user.name "Francisco Ferreira Cardoso"
 
-echo git config -l
-
 #https://stackoverflow.com/questions/10054318/how-to-provide-username-and-password-when-run-git-clone-gitremote-git
 #https://username:password@github.com/username/repository.git
 #https://gist.github.com/willprice/e07efd73fb7f13f917ea
 #git config remote.origin.url 775ce10e820d83ced636f75ebe5b4842abf49179@github.com/totvs/ttalk-standard-message.git
 
 echo "Acessando variavel de ambiente segura"
-echo ${TEST_SECURE}
+echo ${TEST_SECURE} #Prints [secure]
 echo "Testando acessar variavel Travis dentro do SH"
 echo ${TRAVIS_BRANCH}
 
@@ -17,5 +15,5 @@ node validator/master/renameRef.js
 git add -A
 git commit -m "TRAVISCI - Renaming schema references to branch  'master'"
 git remote add origin-pages 775ce10e820d83ced636f75ebe5b4842abf49179@github.com/totvs/ttalk-standard-message.git > /dev/null 2>&1                        
-git push origin $1
+git push origin ${TRAVIS_BRANCH} --quiet
 
