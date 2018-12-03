0435380d655fc3336474dc07c89dff9ce47d957f
Francisco Ferreira Cardoso <francardoso@outlook.com>
2018-12-03 10:55:44 -0200

    Testando accessToken a partir da maquina local

--- travis-renameRef.sh
+++ travis-renameRef.sh
@@ -14,7 +14,7 @@ echo ${GH_TOKEN}
 node validator/master/renameRef.js
 git add -A
 git commit -m "TRAVISCI - Renaming schema references to branch  'master'"
-#git remote add origin-pages 775ce10e820d83ced636f75ebe5b4842abf49179@github.com/totvs/ttalk-standard-message.git > /dev/null 2>&1                        
+#git remote add origin-pages 89d850169e011bab02607657c590d01f0f3e519d@github.com/totvs/ttalk-standard-message.git > /dev/null 2>&1                        
 git remote add origin-pages ${GH_TOKEN}@github.com/totvs/ttalk-standard-message.git > /dev/null 2>&1                        
 git push origin ${TRAVIS_BRANCH}
 #git push origin ${TRAVIS_BRANCH} --quiet
