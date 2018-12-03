8162639dd723620a25dcbad897f4f1a56af8b51f
Francisco Ferreira Cardoso <francardoso@outlook.com>
2018-12-03 15:40:43 -0200

    Nova tentantiva de quebrar responsabilidades dos scripts - mudando tambem o renameRef.js

--- travis-gitCommit.sh
+++ travis-gitCommit.sh
@@ -1,15 +1,13 @@
-echo "travis-gitCommit"
-
-#Francisco
 git config --global user.email "francardoso@outlook.com"
 git config --global user.name "francardoso93"
 
-git remote add origin-pages https://${GH_TOKEN}@github.com/totvs/ttalk-standard-message.git > /dev/null 2>&1     
-git pull
 git checkout ${TRAVIS_BRANCH}
+git pull                   
+
 git add -A
-#git status
-git commit -m "TRAVISCI - Renaming schema references to branch  'master'. Travis build: ${TRAVIS_BUILD_NUMBER}"
-                 
-git push --set-upstream origin-pages ${TRAVIS_BRANCH}
+git status
+git commit -m "TRAVISCI - Renaming schema references to branch  'master' - ${TRAVIS_BUILD_NUMBER}"
+git remote add origin-pages https://${GH_TOKEN}@github.com/totvs/ttalk-standard-message.git > /dev/null 2>&1     
+git push --set-upstream origin-pages 
+
 
