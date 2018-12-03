e4047fd6d5dd088bcdd6d6553e866c5c99bd2ce2
Francisco Ferreira Cardoso <francardoso@outlook.com>
2018-12-03 17:26:00 -0200

    Renomeando schemas e types para a branch master

--- travis-renameRef.sh
+++ travis-renameRef.sh
@@ -4,11 +4,10 @@ git config --global user.name "francardoso93"
 git checkout ${TRAVIS_BRANCH}
 git pull                   
 
-node validator/master/renameRef.js
+node validator/master/renameOpenApiRef.js
+node validator/master/renameSchemaRef.js
 
 git add -A
-git commit -m "TRAVISCI - Renaming schema references to branch  'master' - ${TRAVIS_BUILD_NUMBER}"
+git commit -m "TRAVISCI - Renaming all references to branch 'master'. TravisBuildNumber: ${TRAVIS_BUILD_NUMBER}"
 git remote add origin-pages https://${GH_TOKEN}@github.com/totvs/ttalk-standard-message.git > /dev/null 2>&1     
 git push --set-upstream origin-pages 
-
-
