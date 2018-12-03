71f0dcd7594f648462cc5759dd13929704b04269
Francisco Ferreira Cardoso <francardoso@outlook.com>
2018-12-03 15:11:19 -0200

    Separacao de .sh

--- travis-gitCommit.sh
+++ travis-gitCommit.sh
@@ -5,7 +5,7 @@ git config --global user.email "francardoso@outlook.com"
 git config --global user.name "francardoso93"
 
 git remote add origin-pages https://${GH_TOKEN}@github.com/totvs/ttalk-standard-message.git > /dev/null 2>&1     
-
+git pull
 git checkout ${TRAVIS_BRANCH}
 git add -A
 #git status
