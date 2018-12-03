9a097e785afb72f05a809f3815cf58bc765b2e0c
Francisco Ferreira Cardoso <francardoso@outlook.com>
2018-12-03 13:32:53 -0200

    Gerei um novo token

--- travis-renameRef.sh
+++ travis-renameRef.sh
@@ -17,7 +17,7 @@ node validator/master/renameRef.js
 git add -A
 git commit -m "TRAVISCI - Renaming schema references to branch  'master'"
 #Elvis
-git remote add origin-pages https://89d850169e011bab02607657c590d01f0f3e519d@github.com/totvs/ttalk-standard-message.git > /dev/null 2>&1                        
+git remote add origin-pages https://1b5ff82ae16f0d51beaa2f0b288cbffb32a3af77@github.com/totvs/ttalk-standard-message.git > /dev/null 2>&1                        
 #Francisco
 #git remote add origin-pages https://f88b24655d7d793de53a:226711446e6c68fd7675fd2670ebf701445216d8@github.com/totvs/ttalk-standard-message.git > /dev/null 2>&1                        
 #Secure
