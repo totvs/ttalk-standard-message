d02a94d3bf914d941205875c78795f4f6d93111c
TTalkIntegracoes <TOTVS.TECNOLOGIA.INTEGRACOES@totvs.com.br>
2018-12-10 10:28:29 -0200

    Mudando usuario padrao de push (apos renomear para 'master') e logica do replace para abrangir forks

--- validator/master/commons.js
+++ validator/master/commons.js
@@ -2,7 +2,7 @@ exports.renameRefInternals = function (refObj, refprop) {
     if (refObj[refprop]) {
         if (refObj[refprop].includes("http") && refObj[refprop].includes("totvs/ttalk-standard-message")) {
             refObj[refprop] = refObj[refprop].replace(
-                refObj[refprop].substring(refObj[refprop].lastIndexOf("/totvs/ttalk-standard-message/") + 30, refObj[refprop].lastIndexOf("/jsonschema")), "master"
+                refObj[refprop].substring(0, refObj[refprop].lastIndexOf("/jsonschema")), "https://raw.githubusercontent.com/totvs/ttalk-standard-message/master"
             );
         }
     }
