71155b66a5cfc05b54141ccd8989687a50f372b2
TTalkIntegracoes <TOTVS.TECNOLOGIA.INTEGRACOES@totvs.com.br>
2018-12-10 09:53:15 -0200

    TRAVISCI - Renaming all references to branch 'master'. TravisBuildNumber:

--- validator/master/commons.js
+++ validator/master/commons.js
@@ -3,7 +3,7 @@ exports.renameRefInternals = function (refObj, refprop) {
     if (refObj[refprop]) {
         if (refObj[refprop].includes("http") && refObj[refprop].includes("/jsonschema")) {
             refObj[refprop] = refObj[refprop].replace(
-               0, refObj[refprop].lastIndexOf("/jsonschema"), "https://raw.githubusercontent.com/totvs/ttalk-standard-message/master/"
+               0, refObj[refprop].lastIndexOf("/jsonschema"), "https://raw.githubusercontent.com/totvs/ttalk-standard-message/master"
             );
         }
     }
