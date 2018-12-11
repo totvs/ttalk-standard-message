c65db78d9227495f30458d42df1ebc1271c62c99
TTalkIntegracoes <TOTVS.TECNOLOGIA.INTEGRACOES@totvs.com.br>
2018-12-11 14:46:02 -0200

    Ajuste commons

--- validator/master/commons.js
+++ validator/master/commons.js
@@ -1,6 +1,6 @@
 exports.renameRefInternals = function (refObj, refprop) {
     if (refObj[refprop]) {
-        if (refObj[refprop].includes("http") && refObj[refprop].includes("totvs/ttalk-standard-message")) {
+        if (refObj[refprop].includes("http") && refObj[refprop].includes("/jsonschema")) {
             refObj[refprop] = refObj[refprop].replace(
                 refObj[refprop].substring(0, refObj[refprop].lastIndexOf("/jsonschema")), "https://raw.githubusercontent.com/totvs/ttalk-standard-message/master"
             );
