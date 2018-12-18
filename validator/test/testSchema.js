94c9e7cb6ac3ef4c4d8203d480b1f3cd6fff8ebc
francardoso93 <francardoso@outlook.com>
2018-12-18 15:12:42 -0200

    Ajustes URL pathId

--- validator/test/testSchema.js
+++ validator/test/testSchema.js
@@ -21,7 +21,7 @@ describe("Validating Schema files...", function () {
       // console.log('SCHEMA files');
       // console.log(filenames);
       filenames.forEach(function (filename) {
-        if (filename.includes(".json") && !filename.includes("package")) {
+        if (filename.includes("MaterialFamilies_2_002.json") && !filename.includes("package")) {
           let schemaPath = path.join(dirname, filename);
           var parsedSchema;
           var schemaDefinitionsValidatorResult
