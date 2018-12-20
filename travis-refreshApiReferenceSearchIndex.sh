446ce582d4f170777394ce30c8a74b5f11066c86
francardoso93 <francardoso@outlook.com>
2018-12-20 17:35:34 -0200

    changetest

--- travis-refreshApiReferenceSearchIndex.sh
+++ travis-refreshApiReferenceSearchIndex.sh
@@ -1,3 +1,3 @@
-ECHO 'Refresh API Reference SearchIndex'
+ECHO 'Refresh API Reference SearchIndex aa'
 curl -X POST https://api.totvs.com.br:8027/api/apireference/v1/search/refreshindex -d "Content-Length: 0"
 ECHO ${APIREFERENCEHOST}
