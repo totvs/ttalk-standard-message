3d64c5f3093870d526f178aea1311e841b294037
francardoso93 <francardoso@outlook.com>
2018-12-20 17:33:15 -0200

    RefreshIndex

--- travis-refreshApiReferenceSearchIndex.sh
+++ travis-refreshApiReferenceSearchIndex.sh
@@ -1,5 +1,3 @@
-sudo apt-get update
-sudo apt install curl
 ECHO 'Refresh API Reference SearchIndex'
 curl -X POST https://api.totvs.com.br:8027/api/apireference/v1/search/refreshindex -d "Content-Length: 0"
 ECHO ${APIREFERENCEHOST}
