bf62a60fa486366a4be69f0604a95bf75c77d3c9
francardoso93 <francardoso@outlook.com>
2018-12-21 09:50:00 -0200

    CURL timeout

--- travis-refreshApiReferenceSearchIndex.sh
+++ travis-refreshApiReferenceSearchIndex.sh
@@ -1,1 +1,1 @@
-curl -X POST ${SEARCHINDEXHOST}/api/apireference/v1/search/refreshindex -d "Content-Length: 0"
+curl -X POST ${SEARCHINDEXHOST}/api/apireference/v1/search/refreshindex -d "Content-Length: 0" --max-time 10
