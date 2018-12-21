8bba72f7f616ad260bf3d7ffa462707cd5ae5715
francardoso93 <francardoso@outlook.com>
2018-12-21 09:25:14 -0200

    host em variavel de ambiente

--- travis-refreshApiReferenceSearchIndex.sh
+++ travis-refreshApiReferenceSearchIndex.sh
@@ -1,2 +1,1 @@
-# ECHO 'Refresh API Reference SearchIndex (From .SH)'
-curl -X POST https://api.totvs.com.br:8027/api/apireference/v1/search/refreshindex -d "Content-Length: 0"
+curl -X POST ${SEARCHINDEXHOST}/api/apireference/v1/search/refreshindex -d "Content-Length: 0"
