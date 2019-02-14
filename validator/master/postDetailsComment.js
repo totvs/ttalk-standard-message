c97bd2474a9a693b7edd0395e0bd5976e13c0201
Lucas Reis <reisaolucas@gmail.com>
2019-02-14 17:38:26 -0200

    :end: correct

--- validator/master/postDetailsComment.js
+++ validator/master/postDetailsComment.js
@@ -40,7 +40,7 @@ substr=substr.replace(/\[37;40m/g, '');
 substr=substr.replace(/\[31;40m/g, '');
 substr=substr.replace(/\[2J\[1;3H/g, '');
 substr=substr.replace(/\[0K\[32;1m/g, '');
-substr=substr.replace(/:end:/g, ': end :');
+substr=substr.replace(/:end:/g, ': end:');
 
 var data = "{\n\t\"body\":\""+pretext+substr+"\n"+aftertext+"\"\n}\n";
 
