d968ad6d0e872ab53386005be30a68865887c197
Lucas Reis <reisaolucas@gmail.com>
2019-02-14 14:25:37 -0200

    testing

--- validator/master/postDetailsComment.js
+++ validator/master/postDetailsComment.js
@@ -24,19 +24,27 @@ function getFromUrl(logFile){
 }
 
 var substr = getFromUrl(logFile); //conteúdo armazenado nesta variável
+substr=substr.replace(/\/g, '');
+substr=substr.replace(/\n/g, '');
+substr=substr.replace(/\t/g, '');
+substr=substr.replace(/\//g, '\/');
+substr=substr.replace(/\</g, '\<');
+substr=substr.replace(/\>/g, '\>');
 
+
+console.log(substr);
 //posta o comment
 var data = "{\n\t\"body\":\""+substr+"\"\n}\n";
 
 var xhr = new XMLHttpRequest();
 xhr.withCredentials = true;
 
+xhr.open("POST", "https://api.github.com/repos/totvs/ttalk-standard-message/issues/486/comments", false);
+//xhr.open("POST", "https://api.github.com/repos/totvs/ttalk-standard-message/issues/"+process.env.TRAVIS_PULL_REQUEST+"/comments");
+xhr.setRequestHeader("Authorization", "Bearer 1659148d17bfd880b67771236f72c64cae0dcf53");
 xhr.addEventListener("readystatechange", function () {
   if (this.readyState === 4) {
     console.log(this.responseText);
   }
 });
-
-xhr.open("POST", "https://api.github.com/repos/totvs/ttalk-standard-message/issues/486/comments");
-//xhr.open("POST", "https://api.github.com/repos/totvs/ttalk-standard-message/issues/"+process.env.TRAVIS_PULL_REQUEST+"/comments");
 xhr.send(data);
