c0e6e16b984f6e2a5a75895847552e84e189afe9
Lucas Reis <reisaolucas@gmail.com>
2019-02-14 17:56:32 -0200

    adding one more newline
    \n

--- validator/master/postDetailsComment.js
+++ validator/master/postDetailsComment.js
@@ -19,7 +19,7 @@ function getFromUrl(logFile){
 
 var substr = getFromUrl(logFile);
 var pretext = "A validação foi concluída! Abaixo está evidenciado o resultado do teste:";
-var aftertext = "\\nPara maiores detalhes acesse: https://travis-ci.org/totvs/ttalk-standard-message/builds/"+process.env.TRAVIS_BUILD_ID+"";
+var aftertext = "\\n\\nPara maiores detalhes acesse: https://travis-ci.org/totvs/ttalk-standard-message/builds/"+process.env.TRAVIS_BUILD_ID+"";
 substr=substr.replace(/\/g, '');
 substr=substr.replace(/\n/g, '\\n');
 substr=substr.replace(/\n \n/g, '\\n\\n');
