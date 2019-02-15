fe736389aa36c981ed5ffc4b02a75d9447d98784
Lucas Reis <reisaolucas@gmail.com>
2019-02-15 13:50:38 -0200

    new docReady validation routine

--- validator/master/postDetailsComment.js
+++ validator/master/postDetailsComment.js
@@ -3,8 +3,9 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
 var logFile = "https://api.travis-ci.org/v3/job/"+ process.env.TRAVIS_JOB_ID + "/log.txt";
 
 function getFromUrl(logFile){
-  for (i = 0; i < 100000; i++){
-    if ((substr.match(/npm test/g) || []).length==2) var docReady = true; //check if there are 2 occurences of "npm test" inside txt
+  var docReady = false;
+  while (docReady !=true){
+    if ((substr.match(/npm test/g) || []).length==2) docReady = true; //check if there are 2 occurences of "npm test" inside txt
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", logFile, false);
     rawFile.onreadystatechange = function() {
