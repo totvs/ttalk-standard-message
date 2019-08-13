0715ec41dc41c9a903a6d4486649ba02c3c713a6
francardoso93 <francardoso@outlook.com>
2019-08-13 14:08:43 -0300

    Erro teste coment

--- validator/master/postDetailsComment.js
+++ validator/master/postDetailsComment.js
@@ -1,7 +1,6 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
 
-// var logFile = "https://api.travis-ci.org/v3/job/"+ process.env.TRAVIS_JOB_ID + "/log.txt";
-var logFile = "https://api.travis-ci.org/v3/job/571278253/log.txt";
+var logFile = "https://api.travis-ci.org/v3/job/"+ process.env.TRAVIS_JOB_ID + "/log.txt";
 function getFromUrl(logFile){
   var docReady = false;
   while (docReady!=true){
@@ -22,7 +21,7 @@ function getFromUrl(logFile){
 }
 
 var substr = getFromUrl(logFile);
-var pretext = "A validação foi concluída! Abaixo está evidenciado o resultado do teste:";
+var pretext = "A validação foi concluída! Favor verificar os erros encontrados. Abaixo está evidenciado o resultado do teste:";
 var aftertext = "\\n\\nPara visualizar o log com detalhes da validação que foi realizada, acesse: https://travis-ci.org/totvs/ttalk-standard-message/builds/"+process.env.TRAVIS_BUILD_ID+"";
 var validationDetails = "\\n\\nCaso queira entender melhor cada uma das validações e formas de correção, visite nossa documentação: http://tdn.totvs.com/pages/viewpage.action?pageId=465388996"
 
@@ -54,8 +53,6 @@ substr=substr.replace(/:end:/g, ': end:');
 var data = "{\n\t\"body\":\""+pretext+substr+aftertext+validationDetails+"\"\n}\n";
 var xhr = new XMLHttpRequest();
 xhr.withCredentials = true;
-
-console.log(data);
 
 xhr.open("POST", "https://api.github.com/repos/totvs/ttalk-standard-message/issues/"+process.env.TRAVIS_PULL_REQUEST+"/comments", false);
 xhr.setRequestHeader("Authorization", "Bearer "+process.env.GH_TOKEN+"");
