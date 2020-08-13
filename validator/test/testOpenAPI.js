2ee6ee7e18298a48028ff26390b59b85e747b8db
Francisco Cardoso <francardoso@outlook.com>
2020-08-13 11:00:28 -0300

    Tirar console.log

--- validator/test/testOpenAPI.js
+++ validator/test/testOpenAPI.js
@@ -217,7 +217,6 @@ describe("Validating OpenAPI files...", function () {
                 }
 
                 it("should be dereferenced. This means all external references are correct (FilePaths and Object property names)", function () {
-                  console.log(derefErrorDetail);
                   if (!derefErrorDetail || !derefErrorDetail.ioErrorCode || (typeof derefErrorDetail.ioErrorCode === "string" && !derefErrorDetail.ioErrorCode.includes("ETIMEDOUT"))) { //Não contar como erro de dereference casos de timeout que podem ter sido ocasionados pela rede no Travis
                     expect(derefResult, derefErrorDetail).to.be.ok;
                   }
